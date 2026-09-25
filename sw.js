1e690fdddb61e1e0b7054a89e67bb670ae094a63
nekoyuki35p <nekoyuki36351123@gmail.com>
2026-09-25 23:25:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="dualtube-viewer-v0.5.0";
+const CACHE_NAME="dualtube-viewer-v0.5.1";
 const APP_SHELL=["./","./index.html","./share-target.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
